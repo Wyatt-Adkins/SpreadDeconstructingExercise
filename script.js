f96f92d964a585908e39ce8c918e616b8eb5f5f2
wyattadkins@icloud.com <wyattadkins@icloud.com>
2022-10-17 15:06:37 -0500

    finished work

---
+++ script.js
@@ -0,0 +1,73 @@
+// 1a
+const MCUShows = [`Loki`, `MoonKnight`];
+// 1b
+const StarWarsShows = [`The Mandolorian`, `The Book Of Boba Fett`];
+// 1c
+const DisneyPlusShows = [...MCUShows, ...StarWarsShows];
+// 1d
+console.log (DisneyPlusShows)
+
+// finished with 1
+
+// 2a
+const netflixMovies = {
+    Action: `Extraction`,
+    Crime: `The Irishman`
+};
+// 2b
+const amazonPrimeMovies = {
+    Action: `The Tomorrow War`,
+    Drama: `One Night In Miami`
+};
+// 2c
+const streamingMovies = {
+    ...amazonPrimeMovies, ...netflixMovies, Musical: `Hamilton`,
+};
+// 2d
+console.log (streamingMovies)
+
+// finished with 2
+
+// 3a
+const disneyjunior = [`Mickey Mouse Clubhouse`, `Spidey and his amazing friends`];
+// 3b
+const [Mickey, Spidey] = disneyjunior;
+// 3c
+console.log (disneyjunior)
+
+// finished with 3
+
+// 4a
+const avengers = {
+    Warmachine: `James Rhodes`,
+    theHulk: `Bruce Banner`
+};
+// 4b
+const {Warmachine,thehulk} = avengers;
+// 4c 
+console.log (Warmachine,thehulk);
+// 4d 
+const MoreAvengers = {
+    blackWidow:'Natashsa Romanoff',
+    hawkeye:'Clint Barton',
+    ironman:'Tony Stark'
+};
+// 4e
+const {nat,...others} = MoreAvengers;
+// 4f
+console.log (nat,others)
+
+// bonus
+
+const bonus = {
+    first:[1,2,3],
+    second:[4,5,6],
+    third:[7,8,9]
+};
+
+// 5a
+const all = [...first, ...second, ...third];
+// 5b
+const {one,two,three,four,five,six,seven,eight,nine} = all;
+
+
